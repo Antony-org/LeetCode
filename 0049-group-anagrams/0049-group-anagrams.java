e3b260800eb535bff9e2f1e944651650d43ca2b0
Antony Boutros <51898037+Antony-org@users.noreply.github.com>
2026-06-14 20:34:49 +0300

    Time: 21 ms (10.56%), Space: 51.4 MB (5.83%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -5,10 +5,15 @@ class Solution {
         HashMap<String, List<String>> map = new HashMap<>();
 
         for (String s : strs){
+            int[] alpha = new int[26];
             char[] chars = s.toCharArray();
-            Arrays.sort(chars);
-            String key = new String(chars);
-            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+            
+            for(int i = 0; i < chars.length; i++){
+                alpha[chars[i] - 'a']++;
+            }
+            String numbers = Arrays.toString(alpha);
+
+            map.computeIfAbsent(numbers, k -> new ArrayList<>()).add(s);
         }
         return new ArrayList<>(map.values());
     }
